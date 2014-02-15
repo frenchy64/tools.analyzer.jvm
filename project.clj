21ce71cba74c2bbc5b3fd2b7f94a5c230f8c365e
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-02-15 19:26:40 +0800

    add annotations, before passes

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
-(defproject org.clojure/tools.analyzer.jvm "0.1.0-SNAPSHOT"
+(defproject org.clojure/tools.analyzer.jvm "0.1.0-typed-SNAPSHOT"
   :description "Additional jvm-specific passes for tools.analyzer."
   :url "https://github.com/clojure/tools.analyzer.jvm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
+  :profiles {:dev {:repl-options {:port 64523}}}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.memoize "0.5.6"]
-                 [org.clojure/tools.analyzer "0.1.0-SNAPSHOT"]
+                 [org.clojure/core.typed "0.2.31"]
+                 [org.clojure/tools.analyzer "0.1.0-typed-SNAPSHOT"]
                  [org.ow2.asm/asm-all "4.1"]])
--- src/main/clojure/clojure/tools/analyzer/jvm.clj
+++ src/main/clojure/clojure/tools/analyzer/jvm.clj
@@ -36,8 +36,370 @@
             [clojure.tools.analyzer.passes.jvm.annotate-tag :refer [annotate-tag]]
             [clojure.tools.analyzer.passes.jvm.validate-loop-locals :refer [validate-loop-locals]]
             [clojure.tools.analyzer.passes.jvm.analyze-host-expr :refer [analyze-host-expr]]
-            [clojure.core.memoize :refer [memo-clear!]])
+            [clojure.core.memoize :refer [memo-clear!]]
+            [clojure.core.typed :as t])
   (:import clojure.lang.IObj))
+
+(t/def-alias NsMap
+  (HMap :mandatory
+        {:mappings (t/Map t/Symbol Any) ;contradicts docs, which say (Map Symbol Var). What about Classes?
+         :aliases (t/Map t/Symbol t/Symbol)
+         :ns t/Symbol}))
+
+(t/def-alias Env
+  (HMap :mandatory
+        {:locals (t/Map t/Symbol Expr)
+         :context (U ':return
+                     ':statement
+                     ':expr)
+         :ns t/Symbol
+         :namespaces (t/Atom1 (t/Map t/Symbol NsMap))}
+        :optional
+        {
+        ; ---- start t.a.j specific -----
+        ; added in :deftype
+        ; not sure what :this is
+         :this Any
+        ; ---- end t.a.j specific -----
+         }
+        ))
+
+(t/def-alias Form Any)
+
+(t/def-alias Children (t/Vec Any))
+
+; Notes
+; - :case-test entry added by :case is not here
+
+(t/def-alias Expr
+  (Rec [Expr]
+  ; ----- start tools.analyzer specific ----
+  (U (HMap :mandatory
+           {:op ':const
+            :env Env
+            :type t/Keyword
+            :literal Boolean
+            :val Form
+            :form Form}
+           :optional
+           {:meta Expr
+            :children Children})
+     (HMap :mandatory
+           {:op ':vector
+            :env Env
+            :items (t/Vec Expr)
+            :form Form
+            :children Children})
+     (HMap :mandatory
+           {:op ':with-meta
+            :env Env
+            :form Form
+            :meta Expr
+            :expr Expr
+            :children Children})
+     (HMap :mandatory
+           {:op ':map
+            :env Env
+            :keys (t/Vec Expr)
+            :vals (t/Vec Expr)
+            :form Form
+            :children Children})
+     (HMap :mandatory
+           {:op ':set
+            :env Env
+            :items (t/Vec Expr)
+            :form Form
+            :children Children})
+     (HMap :mandatory
+           {:op ':local
+            :form Form
+            :assignable? Boolean
+            :children Children}
+           :optional
+           ;env is sometimes omitted. eg. :local entry in :fn-method
+           {:env Env})
+     (HMap :mandatory
+           {:op ':var
+            :env Env
+            :form Form
+            :assignable? Boolean
+            :var t/Symbol})
+     (HMap :mandatory
+           {:op ':maybe-host-form
+            :env Env
+            :form Form
+            :class (U nil t/Symbol)
+            :field t/Symbol})
+     (HMap :mandatory
+           {:op ':maybe-class
+            :env Env
+            :form Form
+            ; is this just Symbol?
+            :class (U nil t/Symbol)})
+     (HMap :mandatory
+           {:op ':do
+            :env Env
+            :form Form
+            :statements (t/Vec Expr)
+            :ret Expr
+            :children Children})
+     (HMap :mandatory
+           {:op ':if
+            :env Env
+            :form Form
+            :test Expr
+            :then Expr
+            :else Expr
+            :children Children})
+     (HMap :mandatory
+           {:op ':new
+            :env Env
+            :form Form
+            :class t/Symbol
+            :args (t/Vec Expr)
+            :children Children})
+     (HMap :mandatory
+           {:op ':quote
+            :env Env
+            :form Form
+            :expr Expr
+            :literal? Boolean
+            :children Children})
+     (HMap :mandatory
+           {:op ':set!
+            :env Env
+            :form Form
+            :target Expr
+            :val Expr
+            :children Children})
+     (HMap :mandatory
+           {:op ':try
+            :env Env
+            :form Form
+            :body Expr
+            :catches (t/Vec Expr)
+            :children Children}
+           :optional
+           {:finally Expr})
+     (HMap :mandatory
+           {:op ':binding
+            :env Env
+            :form Form
+            :name t/Symbol
+            :local Any}
+           :optional
+           {:tag (U nil t/Symbol)
+            :arg-id t/Int
+            :variadic? Boolean
+            ;---- start t.a.j specific ----
+            ; not sure what o-tag is
+            :o-tag Any
+            ; added in :deftype
+            :mutable Any
+            ;---- end t.a.j specific ----
+            })
+     (HMap :mandatory
+           {:op ':catch
+            :env Env
+            :form Form
+            :class t/Symbol
+            :local Expr
+            :body Expr
+            :children Children})
+     (HMap :mandatory
+           {:op ':throw
+            :env Env
+            :form Form
+            :exception Expr})
+     (HMap :mandatory
+           {:op ':letfn
+            :env Env
+            :form Form
+            :bindings (t/Vec Expr)
+            :children Children})
+     (HMap :mandatory
+           {:op ':let
+            :form Form
+            :env Env
+            :body Expr
+            :bindings (t/Vec Expr)
+            :children Children})
+     (HMap :mandatory
+           {:op ':loop
+            :form Form
+            :env Env
+            :loop-id t/Symbol
+            :body Expr
+            :bindings (t/Vec Expr)
+            :children Children})
+     (HMap :mandatory
+           {:op ':recur
+            :env Env
+            :form Form
+            :exprs (t/Vec Expr)
+            :loop-id t/Symbol
+            :children Children})
+     (HMap :mandatory
+           {:op ':fn-method
+            :env Env
+            :form Form
+            :loop-id t/Symbol
+            :variadic? Boolean
+            :params (t/Vec Expr)
+            :fixed-arity t/Int
+            :body Expr
+            :children Children}
+           :optional
+           {:local Expr})
+     (HMap :mandatory
+           {:op ':fn
+            :env Env
+            :form Form
+            ;unsure if nilable
+            :name (U nil t/Symbol)
+            :variadic? Boolean
+            :max-fixed-arity (U nil t/Int)
+            :methods (t/Vec Expr)
+            :children Children}
+           :optional
+           {:local Expr})
+     (HMap :mandatory
+           {:op ':def
+            :env Env
+            :form Form
+            :name t/Symbol
+            ;what is the result of create-var?
+            :var Any}
+           :optional
+           {:meta Expr
+            :init Expr
+            :doc String
+            :children Children})
+     (HMap :mandatory
+           {:op ':host-call
+            :env Env
+            :form Form
+            :target Expr
+            :method t/Symbol
+            :args (t/Vec Expr)
+            :children Children})
+     (HMap :mandatory
+           {:op ':host-field
+            :env Env
+            :form Form
+            :target Expr
+            :field t/Symbol
+            :children Children})
+     (HMap :mandatory
+           {:op ':host-interop
+            :env Env
+            :form Form
+            :target Expr
+            :m-or-f t/Symbol
+            :children Children})
+     (HMap :mandatory
+           {:op ':invoke
+            :env Env
+            :form Form
+            :fn Expr
+            :args (t/Vec Expr)
+            :children Children}
+           :optional
+           {:meta Expr})
+; ---- end tools.analyzer specific -----
+           
+; ---- start tools.analyzer.jvm specific ----
+
+     (HMap :mandatory
+           {:op ':the-var
+            :env Env
+            :form Form
+            ; result of resolve-var
+            :var Any})
+     (HMap :mandatory
+           {:op ':monitor-enter
+            :env Env
+            :form Form
+            :target Expr
+            :children Children})
+     (HMap :mandatory
+           {:op ':monitor-exit
+            :env Env
+            :form Form
+            :target Expr
+            :children Children})
+     (HMap :mandatory
+           {:op ':import
+            :env Env
+            :form Form
+            :class t/Symbol})
+     (HMap :mandatory
+           {:op ':method
+            :env Env
+            :form Form
+            :loop-id t/Symbol
+            :params (t/Vec Expr)
+            :fixed-arity t/Int
+            :body Expr
+            :children Children}
+           :optional
+           ; possibly inherited from :fn-method
+           {:local Expr
+            ; added in :reify
+            :interfaces (t/Set (U nil Class))
+            })
+     (HMap :mandatory
+           {:op ':reify
+            :env Env
+            :form Form
+            :class-name t/Symbol
+            :methods (t/Vec Expr)
+            :children Children})
+     (HMap :mandatory
+           {:op ':deftype
+            :env Env
+            :form Form
+            :name t/Symbol
+            :class-name t/Symbol
+            :fields (t/Vec Expr)
+            :methods (t/Vec Expr)
+            ; not sure what :interfaces is
+            :interfaces (t/Set Any)
+            :children Children})
+     (HMap :mandatory
+           {:op ':case-test
+            ;TODO
+            :hash Any
+            :test Expr
+            :children Children})
+     (HMap :mandatory
+           {:op ':case-then
+            ;TODO
+            :hash Any
+            :then Expr
+            :children Children})
+     (HMap :mandatory
+           {:op ':case
+            :env Env
+            :form Form
+            :test Expr
+            :default Expr
+            :tests (t/Vec Expr)
+            :thens (t/Vec Expr)
+            ;TODO
+            :shift Any
+            :mask Any
+            :low Any
+            :high Any
+            :switch-type Any
+            :test-type Any
+            :skip-check? Boolean
+            :children Children})
+
+; ---- end tools.analyzer.jvm specific ----
+
+)))
+
 
 (def specials
   "Set of the special forms for clojure in the JVM"
